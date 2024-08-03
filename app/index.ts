841cf44767829fe3a0b2cd7c61c2d2c6cef5f17a
Keldaan <keldaan.ag@gmail.com>
2024-08-03 18:43:55 +0200

    new config

--- app/index.ts
+++ app/index.ts
@@ -11,6 +11,7 @@
 import { listen } from "@colyseus/tools"
 
 import { RedisDriver, RedisPresence } from "colyseus"
+import { nanoid } from "nanoid"
 // Import Colyseus config
 import app from "./app.config"
 import { initializeMetrics } from "./metrics"
@@ -19,18 +20,11 @@ if (process.env.NODE_APP_INSTANCE) {
   initializeMetrics()
   app.options = {
     presence: new RedisPresence(process.env.REDIS_URI),
-    driver: new RedisDriver(process.env.REDIS_URI)
+    driver: new RedisDriver(process.env.REDIS_URI),
+    publicAddress:
+      process.env.SUBDOMAIN + "." + process.env.SERVER_NAME + "/" + nanoid()
   }
-  let port = 2567
-  const processNumber = Number(process.env.NODE_APP_INSTANCE || "0")
-  port += processNumber
-  // force "publicAddress" when more than 1 process is available
-  app.options.publicAddress =
-    process.env.SUBDOMAIN + "." + process.env.SERVER_NAME
-
-  // nginx is responsible for forwarding /{port}/ to this process
-  app.options.publicAddress += "/" + port
-  listen(app, port)
+  listen(app)
   console.log(app.options)
 } else {
   listen(app, 9000)
